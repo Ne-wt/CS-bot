63bc2e9bdd0ab23c2f4b601fcb1400ee6f633bfb
Newt <dragonminer101@gmail.com>
2021-11-10 13:49:53 +0000

    feat: add info to help command

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const helpers = require("../helpers/helpers.js");
 
 exports.run = async (client, message, args) => {
-    var embedMessage = helpers.embed(client, "```Here's a list of all commands:\n• !help - That's this message :)\n• !ping - Pong!\n ```");
+    var embedMessage = helpers.embed(client, "```Here's a list of all commands:\n• !help - That's this message :)\n• !ping - Pong!\n• !verify [ID] - Verifies you are a first year!```");
     message.channel.send({ embeds: [embedMessage] });
 }
 
