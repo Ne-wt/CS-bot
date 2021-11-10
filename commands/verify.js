825962fb83495df2e82e7f1b1884aa8eb8f0d68e
Newt <dragonminer101@gmail.com>
2021-11-10 13:45:17 +0000

    refactor: edit .env.example and wrap verify in a channel id protection

--- commands/verify.js
+++ commands/verify.js
@@ -7,6 +7,7 @@
  ******************************************************************************************************************************/
 
 const fs = require('fs');
+const env = require('dotenv').config();
 
 exports.run = async (client, message, args) => {
     // check if verified role exists
@@ -14,39 +15,42 @@ exports.run = async (client, message, args) => {
         message.channel.send('Verified role does not exist. Please create it.');
         return;
     }
-    // make sure they sent args
-    if (!args[0]) {
-        message.channel.send('Please provide an id to verify.');
-        return;
-    }
-    // check if the user does not have the 'verified' role
-    if (message.member.roles.cache.find(role => role.name == 'Verified')) {
-        // if so, send an error message
-        return message.channel.send(`You are already verified!`);
-    }
-    // if not, check the id sent by the user and verify them if it exists in student.txt
-    else {
-        // read the file
-        const file = fs.readFileSync('students.txt', 'utf8');
-        // split the file into an array
-        const array = file.split('\n');
-        // check if the id exists in the array
-        if (array.includes(args[0])) {
-            // if so, add the 'verified' role to the user
-            var role = message.guild.roles.cache.find(role => role.name == 'Verified');
-            // add role to user
-            message.member.roles.add(role);
-            // change the verified status in the file
-            const newFile = file.replace(args[0] + '\n', args[0] + ' - verified by ' + message.author.id + ' (' + message.author.username + '#' + message.author.discriminator + ')\n');
-            // write the new file
-            fs.writeFileSync('students.txt', newFile);
-
-            // and send a success message
-            return message.channel.send(`You have been verified!`);
+    // check channel is the verification channel
+    if (message.channel.id != process.env.VERIFICATION_CHANNEL_ID) {
+        // make sure they sent args
+        if (!args[0]) {
+            message.channel.send('Please provide an id to verify.');
+            return;
         }
-        // if not, send an error message
+        // check if the user does not have the 'verified' role
+        if (message.member.roles.cache.find(role => role.name == 'Verified')) {
+            // if so, send an error message
+            return message.channel.send(`You are already verified!`);
+        }
+        // if not, check the id sent by the user and verify them if it exists in student.txt
         else {
-            return message.channel.send(`That ID does not exist in our database! It may have been taken by another member. If you believe this is the case, please contact a member of staff.`);
+            // read the file
+            const file = fs.readFileSync('students.txt', 'utf8');
+            // split the file into an array
+            const array = file.split('\n');
+            // check if the id exists in the array
+            if (array.includes(args[0])) {
+                // if so, add the 'verified' role to the user
+                var role = message.guild.roles.cache.find(role => role.name == 'Verified');
+                // add role to user
+                message.member.roles.add(role);
+                // change the verified status in the file
+                const newFile = file.replace(args[0] + '\n', args[0] + ' - verified by ' + message.author.id + ' (' + message.author.username + '#' + message.author.discriminator + ')\n');
+                // write the new file
+                fs.writeFileSync('students.txt', newFile);
+
+                // and send a success message
+                return message.channel.send(`You have been verified!`);
+            }
+            // if not, send an error message
+            else {
+                return message.channel.send(`That ID does not exist in our database! It may have been taken by another member. If you believe this is the case, please contact a member of staff.`);
+            }
         }
     }
 }
