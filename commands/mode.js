9c70e69c82e93c337854331b0f7f58b0d6d6a04b
Newt <dragonminer101@gmail.com>
2021-11-12 14:26:10 +0000

    feat: add mode command

---
+++ commands/mode.js
@@ -0,0 +1,10 @@
+const env = require('dotenv').config();
+
+exports.run = async (client, message, args) => {
+    message.reply(`Currently in: ${proccess.env.MODE} mode.`);
+}
+
+exports.help = {
+    name: "mode",
+    description: "Displays if bot is in local or prod mode.",
+}
